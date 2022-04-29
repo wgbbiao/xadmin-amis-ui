a114e97c5c3f3e7e7b2d2c82153940b90abc4a4b
guangbiao <wgb237@163.com>
2022-04-29 11:12:15 +0800

    修改用户管理页面

--- pages/auth/add.js
+++ pages/auth/add.js
@@ -19,7 +19,14 @@
       },
       api: {
         url: '/user/add',
-        method: "post"
+        method: "post",
+        data: {
+          password: '${password}',
+          username: '${username}',
+          is_super: '${is_super}',
+          role_ids: '${role_ids|split:","|map:toInt}',
+          permission_ids: '${permission_ids|split:","|map:toInt}',
+        }
       },
       redirect: "/auth/list",
       messages: {
@@ -54,7 +61,68 @@
           label: '是否是超级管理员',
           value: false,
           required: true,
-        }
+        },
+        {
+          type: 'divider'
+        },
+        {
+          type: 'transfer',
+          name: 'role_ids',
+          label: '角色',
+          placeholder: '请选择角色',
+          source: {
+            url: '/role/list',
+            method: 'get',
+            data: {
+              pageSize: 99999,
+              page: 1
+            },
+            adaptor(payload) {
+              let options = [];
+              payload.data.items.forEach(item => {
+                options.push({
+                  label: item.name,
+                  value: item.id
+                })
+              })
+              return {
+                status: 0,
+                data: { options }
+              };
+            }
+          },
+        },
+        {
+          type: 'divider'
+        },
+        {
+          type: 'transfer',
+          name: 'permission_ids',
+          label: '权限',
+          placeholder: '请选择权限',
+          source: {
+            url: '/permission/list',
+            method: 'get',
+            data: {
+              pageSize: 99999,
+              page: 1
+            },
+            adaptor(payload) {
+              let options = [];
+              payload.data.items.forEach(item => {
+                options.push({
+                  label: item.name,
+                  value: item.id
+                })
+              })
+              return {
+                status: 0,
+                data: { options }
+              };
+            }
+
+          },
+        },
       ]
     }]
   }
